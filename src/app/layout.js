b7790db75d55c6982a648fe5e1faf96d7afc7a1b
monophoto <jetsadawut1013@gmail.com>
2023-08-07 16:21:23 +0700

    complet1

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Jetsdawut Papherata"
+            studentId="650610753"
           />
         </div>
       </body>
